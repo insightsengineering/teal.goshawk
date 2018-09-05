b6d9becc78c98176b6440615586dd73d7cb54ccb
Paszty, Nick {MDBT~South San Francisco} <paszty.nicholas@gene.com>
2018-09-06 00:20:42 +0200

    Updated existing code for clarity and added additional arguments to module

--- R/tm_g_scatterplot.R
+++ R/tm_g_scatterplot.R
@@ -24,33 +24,53 @@
 #'\dontrun{
 #' # Example using analysis dataset for example ASL or ADSL,
 #' # ALB points to biomarker data stored in a typical LB structure. for example ALB or ADLB.
-#'
+#' 
 #'# for development team testing
 #'ASL_path <- "~/btk/lupus/dataadam/asl.sas7bdat"
 #'ALB_path <- "~/btk/lupus/dataadam/alb3arm.sas7bdat"
 #'
 #'# list of biomarkers of interest. see ALB2 assignment below
 #'param_choices <- c("CRP","ADIGG","IG","IGA","IGE","IGG","IGM","TEST")
-#'value_var_choices <- c("AVAL","CHG","PCHG")
 #'
-#'ASL <- read_bce(ASL_path)
+#'ASL0 <- read_bce(ASL_path)
+#'ASL <- subset(ASL0, subset = ITTFL == 'Y' & IAFL == 'Y')
+#'
 #'ALB0 <- read_bce(ALB_path)
 #'
-#'# post process the data to subset records per specification and to create new variables
-#'ALB1 <- subset(ALB0,
+#'# post process the data to subset records per specification
+#'ALB_SUBSET <- subset(ALB0,
 #'               subset = PARAMCD %in% c(param_choices) & ITTFL == 'Y' & IAFL == 'Y' & ANLFL == 'Y' & AVISIT %like any% c('BASE%','%WEEK%'), 
-#'               select = c('STUDYID', 'USUBJID', 'ITTFL', 'ANLFL', 'ARM', 'AVISIT', 'AVISITN', 'PARAMCD', 'AVAL', 'AVALU', 'BASE', 'CHG', 'PCHG'))
+#'               select = c('STUDYID', 'USUBJID', 'ITTFL', 'ANLFL', 'ARM', 'AVISIT', 'AVISITN', 'PARAMCD', 'AVAL', 'AVALU', 'BASE', 'CHG', 'PCHG',
+#'                'LBSTRESC', 'LBSTRESN'))
 #'
-#'# create a visit code - baseline record code is "BB" week records coded to "W NN"
-#'ALB <- ALB1 %>% mutate(AVISITCD = paste0(substr(AVISIT,start=1, stop=1), 
+#' # calculate the minimum AVAL for each PARAMCD
+#' PARAM_MINS <- ALB_SUBSET %>%
+#' select(USUBJID, PARAMCD, AVAL) %>%
+#'   filter(PARAMCD %in% param_choices) %>%
+#'   group_by(PARAMCD) %>%
+#'   summarise(AVAL_MIN=min(AVAL, na.rm=TRUE))
+#'   
+#'# post process the data to create several new variables and adjust existing record specific valules per specification
+#'# - create a visit code variable - baseline record code is "BB" and week records coded to "W NN"
+#'# - adjust existing BASELINE record values where values are missing: According to SPA this is a STREAM artifact
+#'ALB_SUPED1 <- ALB_SUBSET %>% mutate(AVISITCD = paste0(substr(AVISIT,start=1, stop=1), 
 #'                                         substr(AVISIT, start=regexpr(" ", AVISIT), stop=regexpr(" ", AVISIT)+2))) %>%
-#'                mutate(AVISITCDN =  ifelse(AVISITCD == "BB", 0, substr(AVISITCD,start=2, stop=4)))
-#'                
-#'ALB$AVISITCDN <- as.numeric(ALB$AVISITCDN) # coerce character into numeric
+#'                mutate(AVISITCDN =  ifelse(AVISITCD == "BB", 0, substr(AVISITCD,start=2, stop=4))) %>%
+#'                mutate(BASE = ifelse(AVISIT == "BASELINE" & is.na(BASE), AVAL, BASE)) %>%
+#'                mutate(CHG = ifelse(AVISIT == "BASELINE" & is.na(CHG), 0, CHG)) %>%
+#'                mutate(PCHG = ifelse(AVISIT == "BASELINE" & is.na(PCHG), 0, PCHG))
+#'                # may need to add similar code for BASE2 related variables
+#'
+#'   
+#' # merge minimum AVAL value onto the ALB data to calculate the log2 variables and preserve the variable order
+#' ALB_SUPED2 <- merge(ALB_SUPED1, PARAM_MINS, by="PARAMCD")[, union(names(ALB_SUPED1), names(PARAM_MINS))] %>%
+#'        mutate(AVALL2 = ifelse(AVAL == 0, log2(AVAL_MIN/2), log2(AVAL))) %>%
+#'        mutate(BASEL2 = ifelse(BASE == 0, log2(AVAL_MIN/2), log2(BASE))) #%>% need SPA to finish adding BASE2 to ALB
+#'        #mutate(BASE2L2 = ifelse(BASE2 == 0, log2(AVAL_MIN/2), log2(AVAL)))
 #'
 #'# for proper chronological ordering of visits in visualizations
-#'ALB <- ALB %>%
-#'       mutate(AVISITCD = factor(AVISITCD) %>% reorder(AVISITCDN))
+#'ALB_SUPED2$AVISITCDN <- as.numeric(ALB_SUPED2$AVISITCDN) # coerce character into numeric
+#'ALB <- ALB_SUPED2 %>% mutate(AVISITCD = factor(AVISITCD) %>% reorder(AVISITCDN))
 #'
 #'
 #' x <- teal::init(
@@ -62,16 +82,17 @@
 #'        param_var = "PARAMCD",
 #'        param_choices = param_choices,
 #'        param = "CRP",
-#'        value_var = "AVAL",
-#'        value_var_choices = c("AVAL", "BASE", "CHG", "PCHG"),
-#'        baseline_var = "BASE",
-#'        baseline_var_choices = c("AVAL", "BASE", "CHG", "PCHG"),
+#'        xaxis_var = "BASE",
+#'        xaxis_var_choices = c("AVAL", "BASE", "CHG", "PCHG", "BASE2", "CHG2", "PCHG2", "AVALL2", "BASEL2", "BASE2L2"),
+#'        yaxis_var = "AVAL",
+#'        yaxis_var_choices = c("AVAL", "BASE", "CHG", "PCHG", "BASE2", "CHG2", "PCHG2", "AVALL2", "BASEL2", "BASE2L2"),
 #'        trt_group = "ARM",
 #'        trt_group_choices = c("ARM", "ARMCD"),
 #'        plot_width = c(800, 200, 2000),
 #'        plot_height = c(800, 200, 2000),
-#'        m_facet = FALSE,
+#'        facet = FALSE,
 #'        reg_line = FALSE,
+#'        font_size = c(12, 8, 20),
 #'        dot_size = c(1, 1, 12)
 #'    )
 #'   )
@@ -86,22 +107,27 @@ tm_g_scatterplot <- function(label, # label of module
                              param_var, # name of variable containing the biomarker names: PARAMCD
                              param, # biomarker selected
                              param_choices = param, # list of biomarkers of interest
-                             baseline_var, # name of variable containing values displayed on the x-axis
-                             baseline_var_choices = baseline_var, # list of baseline variables
-                             value_var, # name of variable containing values displayed on the y-axis
-                             value_var_choices = value_var, # list of analysis variables to plot
+                             xaxis_var, # name of variable containing values displayed on the x-axis
+                             xaxis_var_choices = xaxis_var, # list of baseline variables
+                             yaxis_var, # name of variable containing values displayed on the y-axis
+                             yaxis_var_choices = yaxis_var, # list of analysis variables to plot
                              trt_group,
                              trt_group_choices,
                              plot_width,
                              plot_height,
-                             m_facet = FALSE,
+                             facet = FALSE,
                              reg_line = FALSE,
-                             dot_size, # dot size
+                             font_size,
+                             dot_size,
                              hline = NULL,
                              rotate_xlab = FALSE,
                              man_color = NULL,
                              pre_output = NULL,
-                             post_output = helpText("Scatter Plot Specific Gutter Comment"),
+                             post_output = helpText("BASE2 = Screening Data, BASE2L2 = Log2(BASE2);
+                                                    CHG2 = Change from Screening, PCHG2 =  %Change from Screening;
+                                                    BASE = BASELINE Data, BASEL2 = Log2(BASE);
+                                                    CHG = Change from Baseline, PCHG =  %Change from Baseline;
+                                                    AVAL = Following Visits, AVALL2 = Log2(AVAL)"),
                              code_data_processing = NULL) {
 
   args <- as.list(environment())
@@ -113,8 +139,8 @@ tm_g_scatterplot <- function(label, # label of module
     server_args = list(dataname = dataname,
                        param_var = param_var,
                        param = param,
-                       value_var = value_var,
-                       baseline_var = baseline_var,
+                       xaxis_var = xaxis_var,
+                       yaxis_var = yaxis_var,
                        trt_group = trt_group,
                        code_data_processing = code_data_processing
                        ),
@@ -128,7 +154,7 @@ ui_g_scatterplot <- function(id, ...) {
 
   ns <- NS(id)
   a <- list(...)
-
+print(a)
   standard_layout(
     output = uiOutput(ns("plot_ui")),
     encoding =  div(
@@ -136,14 +162,14 @@ ui_g_scatterplot <- function(id, ...) {
       helpText("Dataset:", tags$code(a$dataname)),
       #helpText("Default Biomarker:", tags$code(a$param)),
       optionalSelectInput(ns("param"), "Select a Biomarker", a$param_choices, a$param, multiple = FALSE),
-      #helpText("Default X-Axis Variable:", tags$code(a$baseline_var)),
-      optionalSelectInput(ns("baseline_var"), "Select an X-Axis Variable", a$baseline_var_choices, a$baseline_var, multiple = FALSE),
-      #helpText("Default Y-Axis Variable:", tags$code(a$value_var)),
-      optionalSelectInput(ns("value_var"), "Select a Y-Axis Variable", a$value_var_choices, a$value_var, multiple = FALSE),
+      #helpText("Default X-Axis Variable:", tags$code(a$xaxis_var)),
+      optionalSelectInput(ns("xaxis_var"), "Select an X-Axis Variable", a$xaxis_var_choices, a$xaxis_var, multiple = FALSE),
+      #helpText("Default Y-Axis Variable:", tags$code(a$yaxis_var)),
+      optionalSelectInput(ns("yaxis_var"), "Select a Y-Axis Variable", a$yaxis_var_choices, a$yaxis_var, multiple = FALSE),
       #optionalSelectInput(ns("trt_group"), "Arm Variable", a$trt_group_choices, a$trt_group, multiple = FALSE),
 
       tags$label("Plot Settings", class="text-primary", style="margin-top: 15px;"),
-      checkboxInput(ns("m_facet"), "Treatment Facetting", a$m_facet),
+      checkboxInput(ns("facet"), "Treatment Facetting", a$facet),
       checkboxInput(ns("reg_line"), "Regression Line", a$reg_line),
       checkboxInput(ns("rotate_xlab"), "Rotate X-axis Label", a$rotate_xlab),
       numericInput(ns("hline"), "Add a horizontal line:", a$hline),
@@ -151,6 +177,7 @@ ui_g_scatterplot <- function(id, ...) {
       optionalSliderInputValMinMax(ns("plot_height"), "Plot Height", a$plot_height, ticks = FALSE),
       uiOutput(ns("xaxis_scale")),
       uiOutput(ns("yaxis_scale")),
+      optionalSliderInputValMinMax(ns("font_size"), "Font Size", a$font_size, ticks = FALSE),
       optionalSliderInputValMinMax(ns("dot_size"), "Dot Size", a$dot_size, ticks = FALSE)
     ),
     forms = tags$div(
@@ -163,7 +190,7 @@ ui_g_scatterplot <- function(id, ...) {
 
 }
 
-srv_g_scatterplot <- function(input, output, session, datasets, dataname, param_var, param, value_var, baseline_var, trt_group, code_data_processing) {
+srv_g_scatterplot <- function(input, output, session, datasets, dataname, param_var, param, xaxis_var, yaxis_var, trt_group, code_data_processing) {
 
   ns <- session$ns # must add for the dynamic ui.range_scale field
   
@@ -185,8 +212,8 @@ srv_g_scatterplot <- function(input, output, session, datasets, dataname, param_
       filter(eval(parse(text = param_var)) == param)
     
       # identify min and max values of BM range ignoring NA values
-      xmin_scale <- min(scale_data[[input$baseline_var]], na.rm = TRUE)
-      xmax_scale <- max(scale_data[[input$baseline_var]], na.rm = TRUE)
+      xmin_scale <- min(scale_data[[input$xaxis_var]], na.rm = TRUE)
+      xmax_scale <- max(scale_data[[input$xaxis_var]], na.rm = TRUE)
 
       tagList({
         sliderInput(ns("xrange_scale"), label="X-Axis Range Scale", xmin_scale, xmax_scale, value = c(xmin_scale, xmax_scale))
@@ -202,8 +229,8 @@ srv_g_scatterplot <- function(input, output, session, datasets, dataname, param_
       filter(eval(parse(text = param_var)) == param)
 
     # identify min and max values of BM range ignoring NA values
-    ymin_scale <- min(scale_data[[input$value_var]], na.rm = TRUE)
-    ymax_scale <- max(scale_data[[input$value_var]], na.rm = TRUE)
+    ymin_scale <- min(scale_data[[input$yaxis_var]], na.rm = TRUE)
+    ymax_scale <- max(scale_data[[input$yaxis_var]], na.rm = TRUE)
 
     tagList({
       sliderInput(ns("yrange_scale"), label="Y-Axis Range Scale", ymin_scale, ymax_scale, value = c(ymin_scale, ymax_scale))
@@ -219,11 +246,12 @@ srv_g_scatterplot <- function(input, output, session, datasets, dataname, param_
     
     ALB <- datasets$get_data(dataname, reactive = TRUE, filtered = TRUE)
     param <- input$param
-    value_var <- input$value_var
-    baseline_var <- input$baseline_var
+    xaxis_var <- input$xaxis_var
+    yaxis_var <- input$yaxis_var
+    font_size <- input$font_size
     dot_size <- input$dot_size
     hline <- as.numeric(input$hline)
-    m_facet <- input$m_facet
+    facet <- input$facet
     reg_line <- input$reg_line
     xmin_scale <- input$xrange_scale[1]
     xmax_scale <- input$xrange_scale[2]
@@ -239,23 +267,26 @@ srv_g_scatterplot <- function(input, output, session, datasets, dataname, param_
                   paste("Biomarker", param, " is not available in data", dataname)))
     validate(need(trt_group %in% names(ALB),
                   paste("variable", trt_group, " is not available in data", dataname)))
-    validate(need(baseline_var %in% names(ALB),
-                  paste("variable", baseline_var, " is not available in data", dataname)))
-    
+    validate(need(xaxis_var %in% names(ALB),
+                  paste("variable", xaxis_var, " is not available in data", dataname)))
+        validate(need(yaxis_var %in% names(ALB),
+                  paste("variable", yaxis_var, " is not available in data", dataname)))
+        
     p <- goshawk:::g_scatterplot(
       data = ALB,
-      biomarker_var = param_var,
-      biomarker = param,
-      value_var = value_var,
-      value_var_bl = baseline_var,
+      param_var = param_var,
+      param = param,
+      xaxis_var = xaxis_var,
+      yaxis_var = yaxis_var,
       trt_group = trt_group,
       color_manual = NULL,
-      f_facet = m_facet,
+      facet = facet,
       reg_line = reg_line,
       xmin_scale = xmin_scale,
       xmax_scale = xmax_scale,
       ymin_scale = ymin_scale,
       ymax_scale = ymax_scale,
+      font_size = font_size,
       dot_size = dot_size,
       rotate_xlab = rotate_xlab,
       hline = hline
