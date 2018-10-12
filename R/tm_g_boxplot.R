12c5d304abf1f771ed53dfdf810b332625f78301
tomlinsj <jeffrey.tomlinson@roche.com>
2018-10-12 09:41:23 +0200

    Update for non-treatment group faceting
    visit_var parameter removed
    facet_var* and xaxis_var* parameters added.
    color_manual and shape_manual parameter reinstated.

--- R/tm_g_boxplot.R
+++ R/tm_g_boxplot.R
@@ -12,17 +12,24 @@
 #' @param param_choices vector of choices to choose the parameter from
 #' @param value_var default columm in \code{dataname} to plot.
 #' @param value_var_choices choice of which columns in \code{dataname} to plot
-#' @param trt_group treatment variable
-#' @param trt_group_choices choices for \code{trt_group}
-#' @param visit_var default columm in \code{dataname} to use for visit.
-#' @param visit_var_choices choice of which columns in \code{dataname} to use
-#'   for visit.  If there is just one value in visit_var_choices, then this will
-#'   be used and no selection of visit_var will be available.
+#' @param trt_group treatment variable.  The boxes and symbols are colored according
+#'    to this parameter.  
+#' @param trt_group_choices choices for \code{trt_group}.  If not specified then
+#'    trt_group cannot be changed.
+#' @param xaxis_var variable to categorize the x-axis
+#' @param xaxis_var_choices choices for \code{xaxis_var}. if not specified then
+#'    \code{xaxis_var} will be fixed and cannot be changed.
+#' @param facet_var variable to facet the plots by.
+#' @param facet_var_choices choices for \code{facet_var}. if not specified then
+#'    \code{facet_var} will be fixed and cannot be changed.
+#' @param armlabel header for the treatment symbols in the legend.  If not specified
+#'    then the label attribute for \code{trt_group} will be used.  If there is 
+#'    no label attribute for \code{trt_group}, then the name of the parameter (
+#'    in title case) will be used.
+#' @param color_manual vector of treatment colors. assigned values in app.R otherwise uses default colors.
+#' @param shape_manual vector of LOQ shapes. assigned values in app.R otherwise uses default shapes.
 #' @param plot_height  numeric vectors to define the plot height.
-#' @param facet boolen - if TRUE facet the display
-#' @param facet_choices boolean - if TRUE display a check box for facetting
 #' @param loq_flag_var variable for the LOQ.  Values are "Y" or "N"
-#' @param code_data_processing TODO
 #' 
 #' @inheritParams teal::standard_layout
 #' 
@@ -89,41 +96,45 @@ tm_g_boxplot <- function(label,
                          value_var_choices = c("AVAL", "CHG"),
                          plot_height = c(600, 200, 2000),
                          trt_group = "ARM",
+                         color_manual = NULL,
+                         shape_manual = NULL,
                          trt_group_choices = NULL,
-                         visit_var = "AVISIT",
-                         visit_var_choices = NULL,
-                         facet_choices = FALSE,
-                         facet = TRUE,
+                         facet_var = "ARM",
+                         facet_var_choices = NULL,
+                         xaxis_var = "AVISIT",
+                         xaxis_var_choices = NULL,
                          loq_flag_var = NULL,
                          pre_output = NULL,
                          post_output = NULL,
+                         armlabel = NULL,
                          code_data_processing = NULL) {
   
   args <- as.list(environment())
   
-  # If there are no choices specified for visit_var then set to visit_var to
-  # enable the default display of the selected visit variable in the UI
-  if (is.null(args$visit_var_choices)) args$visit_var_choices = args$visit_var
-  
-  # If there are no choices specified for treatment group then set the choices 
-  # variable to the treatment group to enable the display of the treatment 
+  # If there are no choices specified for treatment group/x axis/fact then set the 
+  # appropriate choices variable to the treatment group to enable the display of the treatment 
   # group variable on the UI.
   if (is.null(args$trt_group_choices)) args$trt_group_choices = args$trt_group
+  if (is.null(args$xaxis_var_choices)) args$xaxis_var_choices = args$xaxis_var
+  if (is.null(args$facet_var_choices)) args$facet_var_choices = args$facet_var
   
   module(
     label = label,
     filters = dataname,
     server = srv_g_boxplot,
     server_args = list(dataname = dataname,
-                       facet = facet,
-                       facet_choices = facet_choices,
-                       visit_var = visit_var,
-                       visit_var_choices = visit_var_choices,
+                       facet_var = facet_var,
+                       facet_var_choices = facet_var_choices,
+                       xaxis_var = xaxis_var,
+                       xaxis_var_choices = xaxis_var_choices,
                        param_var = param_var,
                        value_var = value_var,
                        trt_group = trt_group,
                        trt_group_choices = trt_group_choices,
+                       color_manual = color_manual,
+                       shape_manual = shape_manual,
                        loq_flag_var = loq_flag_var,
+                       armlabel = armlabel,
                        code_data_processing = code_data_processing
     ),
     ui = ui_g_boxplot,
@@ -159,14 +170,6 @@ ui_g_boxplot <- function(id, ...) {
                           , width = inpWidth
       ),
       
-      optionalSelectInput(ns("visit_var")
-                          , label = "Visit Variable"
-                          , choices = a$visit_var_choices
-                          , selected = a$visit_var
-                          , multiple = FALSE
-                          , width = inpWidth
-      ),
-      
       optionalSelectInput(inputId = ns("param")
                          , label = "Biomarker"
                          , choices = a$param_choices
@@ -185,11 +188,21 @@ ui_g_boxplot <- function(id, ...) {
 
       tags$label("Plot Settings", class="text-primary", style="margin-top: 15px;"),
       
-      # Only display the facet by visit checkbox if faceting choices given.
-      if(a$facet_choices) {
-        checkboxInput(ns("facet"), "Visit Facetting", a$facet)
-      },
+      optionalSelectInput(ns("xaxis_var")
+                          , label = "X-Axis Variable"
+                          , choices = a$xaxis_var_choices
+                          , selected = a$xaxis_var
+                          , multiple = FALSE
+                          , width = inpWidth
+      ),
 
+      optionalSelectInput(ns("facet_var")
+                          , label = "Facet by"
+                          , choices = a$facet_var_choices
+                          , selected = a$facet_var
+                          , multiple = FALSE
+                          , width = inpWidth
+      ),
       optionalSliderInputValMinMax(ns("plot_height")
                                    , label = "Plot height"
                                    , a$plot_height
@@ -233,10 +246,13 @@ ui_g_boxplot <- function(id, ...) {
 }
 
 srv_g_boxplot <- function(input, output, session, datasets
-                          , facet, facet_choices
-                          , visit_var, visit_var_choices
-                          , param_var, value_var, trt_group, trt_group_choices
+                          , facet_var, facet_var_choices
+                          , xaxis_var, xaxis_var_choices
+                          , param_var, value_var
+                          , trt_group, trt_group_choices
+                          , color_manual, shape_manual
                           , loq_flag_var
+                          , armlabel
                           , dataname, code_data_processing) {
   
   chunks <<- list(
@@ -276,7 +292,7 @@ srv_g_boxplot <- function(input, output, session, datasets
   observe({
     output$yaxis_scale <- renderUI({
       param <- input$param # must add for the dynamic ui.range_scale field
-  
+
       # Calculate nice default limits based on the min and max from the data
       lo <- ylimits()$low
       hi <- ylimits()$high
@@ -303,24 +319,45 @@ srv_g_boxplot <- function(input, output, session, datasets
           sliderInput(session$ns("yrange_scale")
                       , label=paste("Y-Axis Range for",param)
                       , ymin_scale, ymax_scale
-                      , value = c(ymin_scale, ymax_scale)
+                      , value = c(ymin_scale, ymax_scale) 
                       )
         })
       }
     })  
   })
   
+  # If facet selection changes to be the same as x axis selection, set the
+  # x axis selection to another value
+  observeEvent(input$facet_var,{
+    x <- input$xaxis_var
+    f <- input$facet_var
+    c <- xaxis_var_choices
+    if (x == f & length(c) > 1) {
+      s <- head(c[c != f],1)
+      updateSelectInput(session,"xaxis_var", selected = s)
+    }
+  })
+
+  observeEvent(input$xaxis_var,{
+    x <- input$xaxis_var
+    f <- input$facet_var
+    c <- facet_var_choices
+    if (x == f & length(c) > 1) {
+      s <- head(c[c != x],1)
+      updateSelectInput(session,"facet_var", selected = s)
+    }
+  })
+  
   output$boxplot <- renderPlot({
     
     filter_var <- input$filter_var
-    trt_group <- ifelse(!is.null(trt_group_choices) , input$trt_group, trt_group)
     value_var <- input$value_var
-    visit_var <- input$visit_var
     param <- input$param
-    if (facet_choices) facet <- input$facet
     dot_size <- input$dot_size
     font_size <- input$font_size
     alpha <- input$alpha
+    xaxis_var <- input$xaxis_var
+    facet_var <- input$facet_var
     
     # Get the filtered data - Filter by both the right an left filters. 
     ASL_FILTERED <- datasets$get_data("ASL", reactive = TRUE, filtered = TRUE)
@@ -340,11 +377,12 @@ srv_g_boxplot <- function(input, output, session, datasets
                   paste("variable", trt_group, " is not available in data", dataname)))
     validate(need(value_var %in% names(ALB),
                   paste("variable", value_var, " is not available in data", dataname)))
+    validate(need(xaxis_var %in% names(ALB),
+                  paste("variable", xaxis_var, " is not available in data", dataname)))
+    validate(need(facet_var %in% names(ALB),
+                  paste("variable", facet_var, " is not available in data", dataname)))
     
     chunks$boxsetup <<- bquote({
-      # If no facet variable has been specified and we're to facet, then set to "None".
-      facet_var <- ifelse(facet == T, visit_var, "None")
-  
       # Units to display, just take the first if there multiples.
       unit <- ALB %>% 
         filter(eval(parse(text = param_var)) == param) %>% 
@@ -369,11 +407,13 @@ srv_g_boxplot <- function(input, output, session, datasets
         ymin_scale = ymin_scale,
         ymax_scale = ymax_scale,
         loq_flag = loq_flag_var, 
-        color_manual = NULL,
-        shape_manual = c('N' = 1, 'Y' = 2, 'NA' = 0),
+        color_manual = color_manual,
+        shape_manual = shape_manual,
         alpha = alpha,
         dot_size = dot_size,
         font_size = font_size,
+        xaxis_var = xaxis_var,
+        armlabel = armlabel,
         facet = facet_var
     )
     
@@ -389,19 +429,7 @@ srv_g_boxplot <- function(input, output, session, datasets
       param <- input$param
       xaxis_var <- input$value_var
       font_size <- input$font_size
-      facet <- ifelse(facet_choices, input$facet, facet)
-      visit_var <- input$visit_var
-      
-      # If the data isn't faceted by visit and there is more than one visit
-      # present in the data, combine the visit data for the summary.
-      chunks$tablesetup <<- bquote({
-        if (!facet) {
-          numVis <- length(unique(ALB[,visit_var]))
-          if (numVis > 1) {
-            ALB[,visit_var] <- "All"
-          }
-        }
-      }) 
+      facet <- ifelse(facet_var_choices, input$facet_var, facet_var)
 
       data_name <- paste0("ALB", "_FILTERED")
       assign(data_name, ALB)
@@ -412,7 +440,7 @@ srv_g_boxplot <- function(input, output, session, datasets
         trt_group = trt_group,
         param_var = param_var,
         param = param,
-        visit_var = visit_var,
+        visit_var = facet_var,
         xaxis_var = xaxis_var, 
         font_size = font_size
       )
@@ -440,8 +468,6 @@ srv_g_boxplot <- function(input, output, session, datasets
        "",
        deparse(chunks$analysis, width.cutoff = 50),
        "\n",
-       teal.tern:::remove_enclosing_curly_braces(deparse(chunks$tablesetup)),
-       "",
        deparse(chunks$table, width.cutoff = 50)
        
      ), collapse = "\n")
