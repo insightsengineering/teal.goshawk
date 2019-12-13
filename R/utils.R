3fffecb04b78bb588a9cce09d6fe6f0057c66a07
Dawid Kałędkowski <dawid.kaledkowski@contractors.roche.com>
2019-12-13 09:30:20 +0100

    remove select yaxis label

--- R/utils.R
+++ R/utils.R
@@ -76,8 +76,12 @@ templ_ui_param <- function(ns, choices, selected) {
 
 templ_ui_xy_vars <- function(ns, xchoices, xselected, ychoices, yselected, multiple = FALSE) {
   tagList(
-    optionalSelectInput(ns("xaxis_var"), "Select an X-Axis Variable",  xchoices,  xselected, multiple = multiple),
-    optionalSelectInput(ns("yaxis_var"), "Select a Y-Axis Variable", ychoices, yselected, multiple = multiple)
+    if (!is.null(xchoices) && !is.null(xselected)) {
+      optionalSelectInput(ns("xaxis_var"), "Select an X-Axis Variable",  xchoices,  xselected, multiple = multiple)
+    },
+    if (!is.null(ychoices) && !is.null(yselected)) {
+      optionalSelectInput(ns("yaxis_var"), "Select a Y-Axis Variable", ychoices, yselected, multiple = multiple)
+    }
   )
 }
 
