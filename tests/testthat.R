160cf576e82f0a4ab08a067bafe6ba85aabb56ad
Dinakar <26552821+dinakar29@users.noreply.github.com>
2021-09-24 16:43:06 -0500

    Enhance CI workflows (#46)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,10 @@
-library(testthat)
-
-test_check("teal.goshawk")
+pkg_name <- "teal.goshawk"
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  reporter <- MultiReporter$new(list(
+    CheckReporter$new(),
+    JunitReporter$new(file = "junit-result.xml")
+  ))
+  test_results <- test_check(pkg_name, reporter = reporter)
+  saveRDS(test_results, "unit_testing_results.rds")
+}
