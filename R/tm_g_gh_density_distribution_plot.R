428f65f74f133eadab6488b70a0a9d2e9ac36f6e
Nikolas Burkoff <nikolas.burkoff@roche.com>
2021-10-04 15:57:38 +0100

    add validation

--- R/tm_g_gh_density_distribution_plot.R
+++ R/tm_g_gh_density_distribution_plot.R
@@ -289,7 +289,9 @@ srv_g_density_distribution_plot <- function(input, # nolint
     line_size <- input$line_size
     hline <- as.numeric(input$hline)
     hline <- `if`(is.na(hline), NULL, as.numeric(hline))
-    facet_ncol <- as.integer(input$facet_ncol)
+    facet_ncol <- input$facet_ncol
+    validate(need(is.na(facet_ncol) || (as.numeric(facet_ncol) > 0 && as.numeric(facet_ncol) %% 1 == 0),
+      "Number of plots per row must be a positive integer"))
     comb_line <- input$comb_line
     rug_plot <- input$rug_plot
     rotate_xlab <- input$rotate_xlab
