a8aad65bb9a9e7c6214e3c0bc1b1fb7e1752d245
Junlue Zhao <jzhao31415@gmail.com>
2021-11-30 10:19:21 -0600

    Update R/tm_g_gh_boxplot.R
    Co-authored-by: Konrad Pagacz <konrad.pagacz@contractors.roche.com>

--- R/tm_g_gh_boxplot.R
+++ R/tm_g_gh_boxplot.R
@@ -363,7 +363,7 @@ ui_g_boxplot <- function(id, ...) {
         div(
           style = "padding: 0px;",
           div(
-            style = "display: inline-block;vertical-align:moddle; width: 100%;",
+            style = "display: inline-block;vertical-align:middle; width: 100%;",
             tags$b("Line Value:")
           ),
           div(
