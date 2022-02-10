fd9ef21e606061ca899cb1095aa32dbdf877e2ae
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-02-10 11:28:37 +0100

    Update tm_g_gh_spaghettiplot.R

--- R/tm_g_gh_spaghettiplot.R
+++ R/tm_g_gh_spaghettiplot.R
@@ -336,7 +336,7 @@ g_ui_spaghettiplot <- function(id, ...) {
             ns("alpha"),
             "Line Alpha",
             a$alpha,
-            value_min_max =  c(0.8, 0.0, 1.0), step = 0.1, ticks = FALSE
+            value_min_max = c(0.8, 0.0, 1.0), step = 0.1, ticks = FALSE
           )
         )
       )
