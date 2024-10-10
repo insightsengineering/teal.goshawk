89ed7c708800eb26037192cd8f842ac3de61c275
m7pr <marcin.kosinski.mk1@roche.com>
2024-10-10 14:06:37 +0200

    introduce log_shiny_input_changes

--- R/tm_g_gh_density_distribution_plot.R
+++ R/tm_g_gh_density_distribution_plot.R
@@ -260,6 +260,7 @@ srv_g_density_distribution_plot <- function(id, # nolint
   checkmate::assert_class(shiny::isolate(data()), "teal_data")
 
   moduleServer(id, function(input, output, session) {
+    teal.logger::log_shiny_input_changes(input, namespace = "teal.goshawk")
     output$axis_selections <- renderUI({
       env <- shiny::isolate(as.list(data()@env))
       resolved_x <- teal.transform::resolve_delayed(module_args$xaxis_var, env)
