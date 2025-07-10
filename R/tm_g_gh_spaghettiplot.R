d8613e3a3d8acf37275950a62b6860c405d7ef80
Marcin <133694481+m7pr@users.noreply.github.com>
2025-07-10 12:22:16 +0200

    Update R/tm_g_gh_spaghettiplot.R
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- R/tm_g_gh_spaghettiplot.R
+++ R/tm_g_gh_spaghettiplot.R
@@ -476,7 +476,7 @@ srv_g_spaghettiplot <- function(id,
       obj <- private_qenv
       teal.reporter::teal_card(obj) <- append(teal.reporter::teal_card(obj), "# Spaghetti Plot", after = 0)
       teal.reporter::teal_card(obj) <- c(teal.reporter::teal_card(obj), "## Plot")
-      obj %>% teal.code::eval_code(
+      teal.code::eval_code(
         object = obj,
         code = bquote({
           p <- goshawk::g_spaghettiplot(
