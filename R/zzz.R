6dbfa8a536eb2b98ed81c34767292b5edee90a81
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-08-17 18:00:49 +0100

    revert: remove function assignment

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,4 @@
 .onLoad <- function(libname, pkgname) {
   teal.logger::register_logger(namespace = "teal.goshawk")
   teal.logger::register_handlers("teal.goshawk")
-
-  if (getRversion() < "4.4") {
-    assign("%||%", rlang::`%||%`, envir = getNamespace(pkgname))
-  }
 }
